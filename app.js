93428175fdfc7a5ff30a72bd94cf69064c044991
DIMITRIDAR <daraseliadimitri@gmail.com>
2023-09-14 15:01:16 +0400

    add the second way to solve task 2

--- app.js
+++ app.js
@@ -47,3 +47,23 @@
 //     12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213,
 //   ])
 // );
+
+//Second way to solve task 2 .
+
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] < arr[j + 1]) {
+//         //Swapping two values here
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(
+//   bubbleSort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213])
+// );
